9a4bb146453a66859a4ef818d2b9d7b02b3544c7
syedhussainfaiq <159991416+syedhussainfaiq@users.noreply.github.com>
2024-04-27 10:29:20 +0500

    my first commit

---
+++ Day 2 challenge/Day 3/inde.ts
@@ -0,0 +1,24 @@
+// Question 7: Number Eight; write addition, subtraction,multipliction,and division operations tht each result in the
+// number 8,Enclose your operations in print statment.
+
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log (16 / 2);
+
+
+// Question 8:You should create four line look like this:
+//console.log(5 + 3)
+
+
+console.log(5 + 3 );
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+
+
+// Question 9:Favorite Number: Store your favorite number in variable. Then, creat message revealing your favorite
+// number.Print that message.
+
+let favoriteNumber : number = 4;
+console.log(`My favorite number is ${favoriteNumber}.`);
---
+++ Day 2 challenge/index.js
@@ -0,0 +1,14 @@
+// Question 1: Famous Quote: Find a quote from a famous person you admire. Print the quote 
+// and the name of its outhor. Your output should look something like the following, including the quotation mark:
+// "Albert Einstien once said,"A person who never made mistake never tried anything new.""
+console.log('Albert Einstein said,"A person who never made a mistake never tried anything new."');
+// questiom 2: Famous Quote 2: Repet Exercise 1,
+var famous_person = "Albert Einstein";
+var message = " ".concat(famous_person, " once said, \"A person who never made a mistake\nnever tried anyrhing new. \"");
+console.log(message);
+// Question 3:Stripping Names: Store a person's name, and include some whitespace characters at the beginning and end of
+//the name. Use "\t"(tab) and "\n"(new line)at least once. Print the name one, so the whitespace around the name is
+//displayed. Then print the name after stripping the whitespaces.
+var Name = "\t\n Faiq \t\n";
+console.log(Name);
+console.log(Name.trim());
