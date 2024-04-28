d79711f1f27447c802d2c5bca96b368df44e98fc
syedhussainfaiq <159991416+syedhussainfaiq@users.noreply.github.com>
2024-04-28 10:04:08 +0500

    my first commit

---
+++ Day 3/main.ts
@@ -0,0 +1,27 @@
+
+
+//Question 7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the
+// number 8. Enclose your operations in print statements.
+
+
+console.log (5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+
+
+//Question 8:** You should create four lines that look like this:
+//console.log(5 + 3)
+
+// Refwer to answer for Question 7, as it same purpose,
+
+
+
+//Question 9:** Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number.
+// Print that message.
+
+
+
+let favoriteNumber: number = 6;
+
+console.log( `My favorite number is ${favoriteNumber}.`);
