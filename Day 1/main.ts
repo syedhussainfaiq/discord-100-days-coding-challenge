e346b19b5151a211bd0a4efcc345f91add3a6783
syedhussainfaiq <159991416+syedhussainfaiq@users.noreply.github.com>
2024-04-28 09:20:06 +0500

    my first commit

---
+++ Day 1/main.ts
@@ -0,0 +1,21 @@
+//Question 1: Install Node.js, TypeScript and VS Code on your computer/Laptop.
+
+
+//Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would
+// you like to learn some TypeScript today?”
+
+
+let Name: string = "Faiq";
+
+console.log(`Hello ${Name}, would you like to learn some Tyypscript today?`);
+
+
+// Question 3:Name Case: Store a person in avariable,and then print that person's in lowercase,Upppercase
+// and titelecase.
+
+
+let  nAme: string = "Faiq";
+
+console.log(nAme.toLocaleLowerCase());
+console.log(nAme.toUpperCase ());
+console.log(nAme.charAt(0).toUpperCase() + nAme.slice(1).toLowerCase());
